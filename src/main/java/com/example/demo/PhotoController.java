9e14e8e3521f4143e6eaf1578458c8e3525688c7
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2024-01-25 11:14:34 -0500

    delete endpoint to controller

--- src/main/java/com/example/demo/PhotoController.java
+++ src/main/java/com/example/demo/PhotoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,11 @@ public class PhotoController {
 
         if (photo == null) {throw new ResponseStatusException(HttpStatus.NOT_FOUND);};
         return photo;
+    }
+
+    @DeleteMapping("/photo/delete/{id}")
+    public void deletePhoto(@PathVariable String id) {
+        Photo photo = db.remove(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
